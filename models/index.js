4cda20af61449702d597bb4fb8561e1d04920498
Sam Craig <samfcraig@gmail.com>
2023-01-20 14:11:42 -0500

    added public/profile.js, commented out. filling out models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,14 @@
 const User = require('./User');
+const Game = require('./Game');
 
 // ASSOCIATIONS HERE
+User.hasMany(Game, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
-module.exports = {
-  User,
-};
+Game.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Game };
