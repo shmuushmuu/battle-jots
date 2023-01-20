0cb1dbc82a2fa69c017943af63a7daa8561006b1
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-01-20 10:53:02 -0500

    fix

--- public/js/battlejots.js
+++ public/js/battlejots.js
@@ -1,1 +1,27 @@
-//this is where the game itself will be coded
+import { WORDS } from "./words";
+
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+console.log(rightGuessString)
+
+function initBoard() {
+    let board = document.getElementById("game-board");
+
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+        
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+}
+
+initBoard()
